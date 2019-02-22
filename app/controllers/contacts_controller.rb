74757008513b33db27a02de95189b9620a4673df
vincentbwalker <vincethewalker@gmail.com>
2019-02-22 13:51:20 +0000

    Added Flash warnings, created show page , added create function in Contacts Controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,28 @@
 class ContactsController < ApplicationController
     
     def new
-       
+       @contact = Contact.new
+    end
+    
+    def create
+       @contact = Contact.new(contact_params)
+       if @contact.save
+           flash[:primary] = "Contact has been saved"
+           redirect_to contact_path(@contact)
+       else
+           flash.now[:danger] = "Error, Try again"
+           render 'new'
+       end
+    end
+    
+    def show
+        
+    end
+    
+    private
+    
+    def contact_params
+       params.require(:contact).permit(:name, :email, :comments) 
     end
     
 end
