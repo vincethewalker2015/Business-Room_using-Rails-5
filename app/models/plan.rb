21d280859df41fb2c3a0a446e7e4f2e44e348501
vincentbwalker <vincethewalker@gmail.com>
2019-02-28 14:29:20 +0000

    Added association for plans and users

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,3 @@
-class Plan < ActiveRecord::Base
-    
+class Plan < ActiveRecord::Base  # Using ActiveRecord::Base (Instead of ActiveRecord)  makes it aware of the database specifically from the seeds.rb file
+    has_many :users
 end
