c19abbd8a539467166b3fbf5d6f630d0831ba442
vincentbwalker <vincethewalker@gmail.com>
2019-03-29 19:35:25 +0000

    Added User-Profile association

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  
   resources :users do
-    resource :profile #Keep it singular as there is only going to be 1 Profile per User
+    resource :profile 
   end
+  
   get 'about', to: 'pages#about'
   get 'plan', to: 'pages#plan'
   resources :contacts, only: :create
